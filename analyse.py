44f7c32b8bd38cf762a324b2c8c1bfbc056d9e26
ShaikSameerHussain2104 <shaiksameerhussain2104@gmail.com>
2025-06-13 11:19:14 +0530

    new data

--- analyse.py
+++ analyse.py
@@ -5,9 +5,9 @@ import numpy as np
 from ultralytics import YOLO
 
 # Load YOLOv5 model (pretrained on COCO dataset)
-# model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-model_path = 'model/yolov5s.pt'
-model = YOLO(model_path)
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+# model_path = 'model/yolov5s.pt'
+# model = YOLO(model_path)
 #Define plastic-related classes
 plastic_classes = ['bottle', 'cup', 'cover', 'wrapper', 'bag', 'can', 'container', 'cell phone', 'remote', 'toilet','polythene']
 
